48d9cb61291e8382b7c7ac5ea5ef8abb2f3e4978
igorko76@gmail.com <igorko76@gmail.com@e936ba92-7213-11de-83e1-4fc93b4a6815>
2009-09-09 18:10:29 +0000

    Initial commit.

---
+++ trunk/src/com/googlecode/simpleret/RunImport.java
@@ -0,0 +1,11 @@
+package com.googlecode.simpleret;
+
+import com.googlecode.simpleret.importer.TraceImporter;
+
+public class RunImport {
+
+	public static void main(String[] args) {
+		TraceImporter.main(args);
+	}
+
+}
