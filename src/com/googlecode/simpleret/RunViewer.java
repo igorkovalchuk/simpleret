8314e477b42f662fe6b0e575714e6f9d6b08fd4e
igorko76 <igorko76@gmail.com>
2009-09-09 18:10:29 +0000

    Initial commit.

---
+++ src/com/googlecode/simpleret/RunViewer.java
@@ -0,0 +1,11 @@
+package com.googlecode.simpleret;
+
+import com.googlecode.simpleret.viewer.Viewer;
+
+public class RunViewer {
+
+	public static void main(String[] args) {
+		Viewer.main();
+	}
+
+}
